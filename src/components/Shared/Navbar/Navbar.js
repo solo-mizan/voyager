2f88b9830165e00602967efce380be8b85b67f4a
solo-mizan <mizanbd.eee@gmail.com>
2023-03-12 23:20:37 +0600

    react-icons installed

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -4,31 +4,38 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 
-
     return (
-        <div class="navbar bg-neutral text-neutral-content">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="navbar bg-base-200">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52">
-                        <li><Link to={'/dashboard'}>Dashboard</Link></li>
-                        <li><Link to={'/manage-profile'}>Manage Profile</Link></li>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Home</a></li>
+                        <li><a>Service</a></li>
+                        <li><a>Top Deals</a></li>
+                        <li><a>Package</a></li>
+                        <li><a>Gallery</a></li>
+                        <li><a>Blog</a></li>
+                        <li><a>Contact</a></li>
                     </ul>
                 </div>
-                <Link to={'/home'} class="btn btn-ghost normal-case text-xl">IELTS Proficiency</Link>
+                <a className="btn btn-ghost normal-case mx-auto text-xl">VOYAGER</a>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal p-0">
-                    <li><NavLink to={'/dashboard'}>Dashboard</NavLink></li>
-                    <li><NavLink to={'/manage-profile'} className="ml-4">Manage Profile</NavLink></li>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li><a>Service</a></li>
+                    <li><a>Top Deals</a></li>
+                    <li><a>Package</a></li>
+                    <li><a>Gallery</a></li>
+                    <li><a>Blog</a></li>
+                    <li><a>Contact</a></li>
                 </ul>
             </div>
-            <div class="navbar-end">
-                {
-                    <Link className='btn' to={'/login'}>Login</Link>
-                }
+            <div className="navbar-end">
+                <a className="btn btn-outline">Login</a>
             </div>
         </div>
     );
