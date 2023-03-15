c9a71753a1c7a5842d2910996a5ab704b08c8581
solo-mizan <mizanbd.eee@gmail.com>
2023-03-16 00:52:41 +0600

    service page added

--- src/components/Pages/Service/Service.js
+++ src/components/Pages/Service/Service.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { HiDotsHorizontal } from 'react-icons/hi';
+import { MdLocalHotel } from 'react-icons/md';
+import { BsFillCircleFill } from 'react-icons/bs';
 
 const Service = () => {
     return (
         <div>
-            <h1>Services are here</h1>
+            <div>
+                <h1 className='banglaFont text-center text-3xl lg:text-4xl text-purple-700'>আমাদের সেবাসমূহ</h1>
+            </div>
+            <div
+                className='text-4xl lg:text-5xl m-1 lg:m-2 text-red-500 justify-center flex'
+            >{<HiDotsHorizontal></HiDotsHorizontal>}</div>
+            <div className='flex items-center justify-center'>
+                <p>এখানে কিছু লেখা থাকবে।।এখানে কিছু লেখা থাকবে।।এখানে কিছু লেখা থাকবে।।এখানে কিছু লেখা থাকবে।।এখানে কিছু লেখা থাকবে।।এখানে কিছু লেখা থাকবে।।এখানে কিছু লেখা থাকবে।।</p>
+            </div>
+            <div>
+                <BsFillCircleFill className='text-7xl text-red-400 z-10'><MdLocalHotel className='z-20 text-3xl text-black'></MdLocalHotel></BsFillCircleFill>
+                <MdLocalHotel className='z-20 text-3xl text-black'></MdLocalHotel>
+            </div>
         </div>
     );
 };
--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -24,30 +24,30 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
-                        <li><a>Service</a></li>
-                        <li><a>Top Deals</a></li>
-                        <li><a>Package</a></li>
-                        <li><a>Gallery</a></li>
-                        <li><a>Blog</a></li>
-                        <li><a>Contact</a></li>
+                        <li><NavLink to='/home'>Home</NavLink></li>
+                        <li><NavLink to='/service'>Service</NavLink></li>
+                        <li><NavLink to='/top-deals'>Top Deals</NavLink></li>
+                        <li><NavLink to='/package'>Package</NavLink></li>
+                        <li><NavLink to='/gallery'>Gallery</NavLink></li>
+                        <li><NavLink to='/blog'>Blog</NavLink></li>
+                        <li><NavLink to='/contact'>Contact</NavLink></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case mx-auto text-xl">VOYAGER</a>
+                <Link to='/home' className="btn btn-ghost normal-case mx-auto text-xl">VOYAGER</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
-                    <li><a>Service</a></li>
-                    <li><a>Top Deals</a></li>
-                    <li><a>Package</a></li>
-                    <li><a>Gallery</a></li>
-                    <li><a>Blog</a></li>
-                    <li><a>Contact</a></li>
+                    <li><NavLink to='/home'>Home</NavLink></li>
+                    <li><NavLink to='/service'>Service</NavLink></li>
+                    <li><NavLink to='/top-deals'>Top Deals</NavLink></li>
+                    <li><NavLink to='/package'>Package</NavLink></li>
+                    <li><NavLink to='/gallery'>Gallery</NavLink></li>
+                    <li><NavLink to='/blog'>Blog</NavLink></li>
+                    <li><NavLink to='/contact'>Contact</NavLink></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn btn-outline">Login</a>
+                <Link to='/login' className="btn btn-outline">Login</Link>
             </div>
         </div>
     );
